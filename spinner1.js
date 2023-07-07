50cc49dc197804fc6451e5faa70ef1de3660e5fe
Joey Chan <joeychan916@gmail.com>
2023-07-07 22:32:55 +0000

    Complete spinner animation

---
+++ spinner1.js
@@ -0,0 +1,54 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+
+// Add the rest of the characters to complete one full animation
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1900);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 2100);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 2300);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 2500);
+
+// until completing a full circle
